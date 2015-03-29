8eb70a9de9b9f48da8ab2061cc901316fdfd4747
张敏华 <zhangminhua@wondersgroup.com>
2015-03-29 17:58:53 +0800

    实现图片的缩放和移动，并且在过程中检测边界的情况

--- src/com/example/ImageGesture/MyActivity.java
+++ src/com/example/ImageGesture/MyActivity.java
@@ -1,7 +1,10 @@
 package com.example.ImageGesture;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +14,13 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    public void showDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(LayoutInflater.from(this).inflate(R.layout.main,null));
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
